1dc0ce4f4a47421fadd517cbe0bf3420b812671a
Sam Banankhu <sambanankhumhango@gmail.com>
2022-09-09 00:24:31 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, DateTime, Integer, create_engine
+from datetime import datetime
+import os
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+connection_string = 'sqlite:///'+os.path.join(BASE_DIR, 'site.db')
+
+Base = declarative_base()
+engine = create_engine(connection_string, echo=True)
+
+"""
+class user
+    id int
+    username str
+    email str
+    datee_created datetime
+"""
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer(), primary_key = True)
+    username = Column(String(25), nullable = False, unique = True)
+    email = Column(String(80), unique=False, nullable=False)
+    date_created = Column(DateTime(), default = datetime.utcnow)
+
+    def __repr__(self):
+        return f'<User username = {self.username} email = {self.email}>'
+
+
+new_user = User(id=1, username='Sam', email='sam@test.com')
+print(new_user)
